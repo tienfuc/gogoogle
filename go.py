b362af8fd15b6b65f399a63a7c6704f751863ba5
ctf <tienfu.c@gmail.com>
2015-04-06 19:47:51 +0800

    move to function

--- go.py
+++ go.py
@@ -7,106 +7,145 @@ from time import sleep
 from user import USER, PASSWORD, PROJECT, FOLDER
 
 import sys
-#from IPython.core import ultratb
-#sys.excepthook = ultratb.FormattedTB(mode='Verbose', color_scheme='Linux', call_pdb=1)
 
-display = Display(visible=0, size=(800, 600))
-display.start()
 
-# download json credential file without ask
-profile = webdriver.FirefoxProfile()
-profile.set_preference('browser.download.folderList', 2) # custom location
-profile.set_preference('browser.download.manager.showWhenStarting', False)
-profile.set_preference('browser.download.dir', FOLDER)
-profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/json')
+def run():
+    global display 
+    display = Display(visible=0, size=(800, 600))
+    display.start()
 
-driver = webdriver.Firefox(profile)
+    # download json credential file without ask
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference('browser.download.folderList', 2) # custom location
+    profile.set_preference('browser.download.manager.showWhenStarting', False)
+    profile.set_preference('browser.download.dir', FOLDER)
+    profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/json')
 
-url_console = "https://console.developers.google.com/project"
-url_googlelogin = "https://accounts.google.com/ServiceLogin?continue=" + quote_plus(url_console)
+    global driver
+    driver = webdriver.Firefox(profile)
 
-driver.get(url_googlelogin)
+    url_console = "https://console.developers.google.com/project"
+    url_googlelogin = "https://accounts.google.com/ServiceLogin?continue=" + quote_plus(url_console)
 
-# login
-driver.find_element_by_id("Email").send_keys(USER)
-driver.find_element_by_id("Passwd").send_keys(PASSWORD)                                                            
-driver.find_element_by_id("signIn").click()    
+    driver.get(url_googlelogin)
+    sleep(2)
 
-if url_console != driver.current_url:
-    raise Exception("Login failed")
-else:
-    print "Login ok"
+    # login
+    driver.find_element_by_id("Email").send_keys(USER)
+    driver.find_element_by_id("Passwd").send_keys(PASSWORD)                                                            
+    driver.find_element_by_id("signIn").click()    
+    sleep(2)
 
-sleep(3)
-driver.find_element_by_id("projects-create").click()
-driver.find_element_by_name("name").send_keys(PROJECT)
-sleep(3)
-driver.find_element_by_name("ok").click()
-print "creating project: %s" % PROJECT
-# wait for project creation
-sleep(30)
+    if url_console != driver.current_url:
+        raise Exception("Login failed")
+    else:
+        print "Login ok"
 
-print "project creation done: %s" % PROJECT
+    projects = driver.find_elements_by_css_selector("a[class=\"p6n-clickable-link ng-binding ng-scope\"]")
+    for p in projects:
+        href = p.get_attribute("href")
+        print href
+        if PROJECT in href:
+            print "Project existed: %s" % PROJECT
+        else:
+            driver.find_element_by_id("projects-create").click()
+            driver.find_element_by_name("name").send_keys(PROJECT)
+            sleep(2)
+            driver.find_element_by_name("ok").click()
+            print "Creating project: %s" % PROJECT
 
-application_name = driver.find_element_by_css_selector("b[class=\"ng-binding\"]").text
+            # wait for project creation
+            max_retries = 30
+            while True:
+                sleep(1)
+                try:
+                    application_name = driver.find_element_by_css_selector("b[class=\"ng-binding\"]").text
+                    print application_name
+                    print PROJECT
+                    if PROJECT == application_name:
+                        print "project creation done: %s" % PROJECT
+                        break
+                except:
+                    max_retries -= 1
+                    pass
 
-# page consent
-url_project = url_console + "/" + PROJECT + "/apiui/consent"
-driver.get(url_project)
-sleep(3)
+                if max_retries < 0:
+                    print "xxx"
+                    raise Exception("Project creation failed: %s" % PROJECT)
 
-# email
-driver.find_element_by_css_selector('div[class="goog-inline-block goog-flat-menu-button-caption"]').click()
-driver.find_element_by_class_name("goog-menuitem").click()
+    # page consent
+    url_project = url_console + "/" + PROJECT + "/apiui/consent"
+    driver.get(url_project)
+    sleep(3)
 
-# product name
-driver.find_element_by_name("displayName").send_keys("application")
-driver.find_element_by_id("api-consent-save").click()
+    # email
+    driver.find_element_by_css_selector('div[class="goog-inline-block goog-flat-menu-button-caption"]').click()
+    driver.find_element_by_class_name("goog-menuitem").click()
 
-print "consent ok"
+    # product name
+    driver.find_element_by_name("displayName").send_keys("application")
+    driver.find_element_by_id("api-consent-save").click()
 
-# page credential
-url_credential = url_console + "/" + PROJECT + "/apiui/credential"
-driver.get(url_credential)
-sleep(5)
+    print "consent ok"
 
-# Create new Client ID
-driver.find_element_by_css_selector('jfk-button[jfk-button-style="PRIMARY"]').click()
-sleep(3)
+    # page credential
+    url_credential = url_console + "/" + PROJECT + "/apiui/credential"
+    driver.get(url_credential)
+    sleep(5)
 
-# Installed application
-txt_application = "Installed application"
-label_application = driver.find_elements_by_css_selector('label')[2]
-if txt_application in label_application.text:
-    label_application.click()
-else:
-    raise Exception("lable not found: %s" % txt_application)
+    # Create new Client ID
+    driver.find_element_by_css_selector('jfk-button[jfk-button-style="PRIMARY"]').click()
+    sleep(3)
 
-# Other
-txt_other = "Other"
-label_other = driver.find_elements_by_css_selector('label')[7]
-if txt_other in label_other.text:
-    label_other.click()
-else:
-    raise Exception("lable not found: %s" % txt_other)
+    # Installed application
+    txt_application = "Installed application"
+    label_application = driver.find_elements_by_css_selector('label')[2]
+    if txt_application in label_application.text:
+        label_application.click()
+    else:
+        raise Exception("lable not found: %s" % txt_application)
 
-# OK
-driver.find_element_by_name("ok").click()
+    # Other
+    txt_other = "Other"
+    label_other = driver.find_elements_by_css_selector('label')[7]
+    if txt_other in label_other.text:
+        label_other.click()
+    else:
+        raise Exception("lable not found: %s" % txt_other)
 
-print "credential ok"
+    # OK
+    driver.find_element_by_name("ok").click()
 
-sleep(3)
+    print "credential ok"
 
-# json URL
-url_downloadjson = driver.find_element_by_css_selector('a[class="goog-inline-block jfk-button jfk-button-standard ng-scope"]').get_attribute("href")
-print "Downloading: %s" % url_downloadjson
-# get json file
-driver.get(url_downloadjson)
+    sleep(3)
 
-s = re.search(r'clientId=([^&]+)', url_downloadjson, flags=re.I)
-credential_file = os.path.join(FOLDER, "client_secret_"+s.group(1)+".json")
+    # json URL
+    url_downloadjson = driver.find_element_by_css_selector('a[class="goog-inline-block jfk-button jfk-button-standard ng-scope"]').get_attribute("href")
+    print "Downloading: %s" % url_downloadjson
+    # get json file
+    driver.get(url_downloadjson)
 
-print "Credential json file: %s" % credential_file
+    print "Credential json file: %s" % credential_file
+    s = re.search(r'clientId=([^&]+)', url_downloadjson, flags=re.I)
+    client_id = s.group(1)
+    credential_file = os.path.join(FOLDER, "client_secret_"+client_id+".json")
+    f = open(credential_file, 'fb')
+    if client_id in f.read():
+        print "Credential json file is valid"
+    else:
+        raise Exception("Credential json file is invalid")
 
-driver.quit()
-display.stop()
+
+def unload():
+    print "Unloading webdriver and virtualdisplay"
+    driver.quit()
+    display.stop()
+
+if __name__ == "__main__":
+    try:
+        run()
+        unload()
+    except:
+        raise
+        unload()
